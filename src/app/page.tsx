fbf44654609e59c137c61a0b864bad321867684a
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2025-07-16 23:07:01 +0700

    todo-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,147 @@
 import Image from "next/image";
+import { FC } from "react";
+
+const dummyData = [
+  {
+    name: "Complete online JavaScript course",
+    isChecked: true,
+  },
+  {
+    name: "Jog around the park 3x",
+    isChecked: false,
+  },
+  {
+    name: "10 minutes meditation",
+    isChecked: false,
+  },
+  {
+    name: "Read for 1 hour",
+    isChecked: false,
+  },
+  {
+    name: "Pick up groceries",
+    isChecked: false,
+  },
+  {
+    name: "Complete Todo App on Frontend Mentor",
+    isChecked: false,
+  },
+];
+
+interface TodoProps {
+  todo: { name: string; isChecked: boolean };
+}
+
+const Todo: FC<TodoProps> = ({ todo }) => {
+  const { name, isChecked } = todo;
+
+  return (
+    <div className="p-[20px_24px] shadow-[0px_1px_0px_0px_#E3E4F1] cursor-pointer">
+      <label className="flex items-center w-full">
+        <input type="checkbox" className="appearance-none" />
+
+        <span className="flex flex-row items-center gap-6">
+          <span
+            className={`inline-block w-6 h-6 rounded-full ${
+              isChecked
+                ? "bg-[linear-gradient(135deg,_#55DDFF_0%,_#C058F3_100%)]"
+                : "bg-transparent"
+            }`}
+          >
+            <span className="flex items-center justify-center w-full h-full">
+              <span className="inline-block relative w-[11.7px] h-[9px]">
+                <Image
+                  src="/static/ic-check.svg"
+                  alt="Check Icon"
+                  fill
+                  className="object-contain"
+                />
+              </span>
+            </span>
+          </span>
+
+          <span
+            className={`m-0 text-lg font-normal ${
+              isChecked ? "line-through text-[#D1D2DA]" : ""
+            }`}
+          >
+            {name}
+          </span>
+        </span>
+      </label>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="w-full h-full relative py-[70px]">
+      <div className="w-full h-full max-w-[541px] relative z-10 mx-auto">
+        {/* Judul Todo */}
+        <div className="flex items-center justify-center mb-10">
+          <div className="w-full h-full flex flex-row items-center justify-between gap-6">
+            <h1 className="text-[40px] font-bold tracking-[15px] leading-normal text-white m-0">
+              TODO
+            </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="relative w-[25.11px] h-[26px]">
+              <Image
+                src="/static/ic-moon.svg"
+                alt="Icon Moon"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        {/* Input Todo */}
+        <div className="mb-6 shadow-[0p_35px_50px_-15px_#C2C3D680]">
+          <input
+            type="text"
+            placeholder="Create a new todo…"
+            className="bg-white w-full rounded-[5px] p-[20px_24px] text-lg"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+
+        {/* List Todo */}
+        <div className="w-full bg-white rounded-[5px] overflow-hidden shadow-[0px_35px_50px_-15px_#C2C3D680]">
+          {dummyData.map((todo) => (
+            <Todo key={todo.name} todo={todo} />
+          ))}
+
+          <div className="p-[16px_24px] flex flex-row items-center justify-between">
+            <p className="m-0 text-sm font-normal text-[#9495A5]">
+              {dummyData.length} items left
+            </p>
+
+            <div className="flex flex-row items-center gap-[19px]">
+              <p className="m-0 text-sm font-bold text-[#9495A5]">All</p>
+              <p className="m-0 text-sm font-bold text-[#9495A5]">Active</p>
+              <p className="m-0 text-sm font-bold text-[#9495A5]">Completed</p>
+            </div>
+
+            <div>
+              <p className="m-0 text-sm font-normal text-[#9495A5]">
+                Clear Completed
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute w-full h-full top-0 left-0 z-0">
+        <div className="w-full h-[300px] relative">
+          <div className="relative w-full h-full bg-[linear-gradient(225deg,_rgba(85,_150,_255,_0.8)_0%,_rgba(172,_45,_235,_0.8)_100%)] z-10" />
+
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/static/bg-image.png"
+            alt="Background Image"
+            fill
+            className="object-cover z-0"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
 }
